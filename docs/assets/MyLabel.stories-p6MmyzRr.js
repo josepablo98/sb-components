c4d7be2017e6fcf04f911e75594fe3df46a544c7
Jose Pablo <josepabloruizperez99@gmail.com>
2023-11-28 23:58:26 +0100

    commit segundo

--- docs/assets/MyLabel.stories-p6MmyzRr.js
+++ docs/assets/MyLabel.stories-p6MmyzRr.js
@@ -2,7 +2,7 @@ import{j as v}from"./jsx-runtime-vNq4Oc-g.js";import"./index-4g5l5LRQ.js";import
 allCaps: boolean, (si true, todo mayuscula, si false, normal) ✅
 color: 'text-primary' | 'text-secondary' | 'text-tertiary', ✅
 fontColor: string (se debe aplicar al color del texto del span) ✅
-doc auto`,displayName:"MyLabel",props:{label:{defaultValue:null,description:"Text to display",name:"label",required:!0,type:{name:"string"}},size:{defaultValue:null,description:"Label size",name:"size",required:!1,type:{name:"enum",value:[{value:'"normal"'},{value:'"h1"'},{value:'"h2"'},{value:'"h3"'}]}},allCaps:{defaultValue:null,description:"All Capitalize",name:"allCaps",required:!1,type:{name:"boolean"}},color:{defaultValue:null,description:"Color",name:"color",required:!1,type:{name:"enum",value:[{value:'"text-primary"'},{value:'"text-secondary"'},{value:'"text-tertiary"'}]}},fontColor:{defaultValue:null,description:"Text font",name:"fontColor",required:!1,type:{name:"string"}}}}}catch{}const z={title:"UI/labels/MyLabel",component:s,tags:["autodocs"],parameters:{layout:"centered"},argTypes:{size:{control:"inline-radio"},fontColor:{control:"color"}}},e={args:{label:"Basic Label"}},a={args:{label:"All Caps Label",allCaps:!0}},r={args:{label:"Secondary Label",color:"text-secondary"}},o={args:{label:"Custom Color Label",color:"text-tertiary"}};var t,n,c;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+doc auto ✅`,displayName:"MyLabel",props:{label:{defaultValue:null,description:"Text to display",name:"label",required:!0,type:{name:"string"}},size:{defaultValue:null,description:"Label size",name:"size",required:!1,type:{name:"enum",value:[{value:'"normal"'},{value:'"h1"'},{value:'"h2"'},{value:'"h3"'}]}},allCaps:{defaultValue:null,description:"All Capitalize",name:"allCaps",required:!1,type:{name:"boolean"}},color:{defaultValue:null,description:"Color",name:"color",required:!1,type:{name:"enum",value:[{value:'"text-primary"'},{value:'"text-secondary"'},{value:'"text-tertiary"'}]}},fontColor:{defaultValue:null,description:"Text font",name:"fontColor",required:!1,type:{name:"string"}}}}}catch{}const z={title:"UI/labels/MyLabel",component:s,tags:["autodocs"],parameters:{layout:"centered"},argTypes:{size:{control:"inline-radio"},fontColor:{control:"color"}}},e={args:{label:"Basic Label"}},a={args:{label:"All Caps Label",allCaps:!0}},r={args:{label:"Secondary Label",color:"text-secondary"}},o={args:{label:"Custom Color Label",color:"text-tertiary"}};var t,n,c;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
   args: {
     label: "Basic Label"
   }
@@ -14,11 +14,11 @@ doc auto`,displayName:"MyLabel",props:{label:{defaultValue:null,description:"Tex
 }`,...(p=(i=a.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var d,m,y;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
   args: {
     label: "Secondary Label",
-    color: 'text-secondary'
+    color: "text-secondary"
   }
 }`,...(y=(m=r.parameters)==null?void 0:m.docs)==null?void 0:y.source}}};var b,C,g;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`{
   args: {
-    label: 'Custom Color Label',
-    color: 'text-tertiary'
+    label: "Custom Color Label",
+    color: "text-tertiary"
   }
 }`,...(g=(C=o.parameters)==null?void 0:C.docs)==null?void 0:g.source}}};const A=["Basic","AllCaps","Secondary","CustomColor"];export{a as AllCaps,e as Basic,o as CustomColor,r as Secondary,A as __namedExportsOrder,z as default};
